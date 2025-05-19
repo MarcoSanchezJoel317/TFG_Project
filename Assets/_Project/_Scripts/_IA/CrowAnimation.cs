0d3da5d91577c5d7bff038c70248614ae61510ec
MarcoSanchezJ <marcosanchezjoel@gmail.com>
2025-05-19 13:28:23 +0200

    Basic Crow

---
+++ Assets/_Project/_Scripts/_IA/CrowAnimation.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class CrowAnimation : MonoBehaviour
+{
+    [Header("Transform settings")]
+    [SerializeField] float maxAltitude = 6;
+    [SerializeField] float minAltitude = 1;
+
+    [Header("Animation controler")]
+    Animator animator;
+
+    [Header("AIconection")]
+    [SerializeField] CrowAI crowAI;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        crowAI = transform.parent.gameObject.GetComponent<CrowAI>();
+    }
+
+    private void Update()
+    {
+        if (crowAI.detected)
+        {
+            animator.SetBool("Attacking", true);
+            if (crowAI.agent.remainingDistance < 5f)
+                ChangeAltitude(minAltitude);
+        }
+        else if (animator.GetBool("Attacking"))
+        {
+            animator.SetBool("Attacking", false);
+            ChangeAltitude(maxAltitude);
+        }
+    }
+
+    void ChangeAltitude(float targetAltitude)
+    {
+        StopAllCoroutines(); // Detiene cualquier cambio anterior en progreso       
+
+        StartCoroutine(ChangeAltitudeCoroutine(targetAltitude));
+    }
+
+    IEnumerator ChangeAltitudeCoroutine(float targetAltitude)
+    {
+        float speed = 2f;
+
+        if (transform.localPosition.y < minAltitude)
+            transform.localPosition = new Vector3(transform.localPosition.x, minAltitude, transform.localPosition.z);
+        else if (transform.localPosition.y > maxAltitude)
+
+            transform.localPosition = new Vector3(transform.localPosition.x, maxAltitude, transform.localPosition.z);
+        while (transform.localPosition.y >= minAltitude && transform.localPosition.y <= maxAltitude)
+        {
+            float newY = Mathf.MoveTowards(transform.localPosition.y, targetAltitude, speed * Time.deltaTime);
+            transform.localPosition = new Vector3(transform.localPosition.x, newY, transform.localPosition.z);
+
+            yield return null;
+        }        
+    }
+
+}
